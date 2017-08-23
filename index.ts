0cfed75118622fde67f5c1383bdfcf6bea6d28da
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-24 01:42:27 +0300

    fix

--- index.ts
+++ index.ts
@@ -10,101 +10,97 @@ export const load = (jumpFm: JumpFm) => {
     const dialog = jumpFm.dialog
     const clipboard = jumpFm.electron.clipboard
     const shell = jumpFm.electron.shell
-    const bind = jumpFm.bindKeys
-    const activePanel = jumpFm.getActivePanel
-    const passivePanel = jumpFm.getPassivePanel
 
-    const del = () =>
-        activePanel().getSelectedItemsPaths().forEach(path => fs.remove(path))
-
-    const edit = () =>
-        cmd.run(
-            jumpFm.settings.getStr('editor', 'gedit')
-            + " "
-            + activePanel().getCurItem().path
-        )
-
-    const newFile = () => {
-        const pwd = activePanel().getPath()
-        jumpFm.dialog.open({
-            label: "New File",
-            onOpen: input => {
-                input.value = "untitled.txt"
-                input.select();
-            },
-            onAccept: name => {
-                const f = path.join(pwd, name)
-                fs.closeSync(fs.openSync(f, 'a'))
-                shell.openItem(f);
-            }
+    jumpFm.panels.forEach(panel => {
+        panel.bind('del', ['del'], () => {
+            console.log('del')
+            panel.getSelectedItems().forEach(item => {
+                fs.remove(item.path)
+            })
         })
-    }
 
-    const rename = () => {
-        dialog.open({
-            label: 'Rename',
-            onOpen: input => {
-                console.log('open', input)
-                const name = activePanel().getCurItem().name
-                input.value = name
-                input.select()
-                input.setSelectionRange(0, name.length - path.extname(name).length);
-            },
-            onAccept: name => {
-                const pan = activePanel()
-                fs.renameSync(
-                    pan.getCurItem().path,
-                    path.join(pan.getPath(), name)
-                )
-            },
-        })
-    }
-
-    const newFolder = () => {
-        dialog.open({
-            label: 'New Folder',
-            onOpen: input => {
-                input.value = 'New Folder'
-                input.select()
-            },
-            onAccept: name => {
-                const pan = activePanel()
-                fs.mkdirSync(
-                    path.join(pan.getPath(), name)
-                )
-            },
-        })
-    }
-
-    const mv = () => {
-        activePanel().getSelectedItems().forEach(file =>
-            fs.move(
-                file.path,
-                path.join(passivePanel().getPath(), file.name)
+        panel.bind('edit', ['f4'], () => {
+            cmd.run(
+                jumpFm.settings.getStr('editor', 'gedit')
+                + " "
+                + panel.getCurrentItem().path
             )
-        )
-    }
+        })
 
-    const copyPath = (path: string) => {
-        clipboard.writeText(path)
-        jumpFm.statusBar.info('clipboard', {
-            txt: 'Path Copied',
-            dataTitle: path
-        }, 6000)
-    }
+        panel.bind('newFile', ['shift+f4'], () => {
+            const pwd = panel.getUrl().path
+            jumpFm.dialog.open({
+                label: 'New File',
+                onOpen: input => {
+                    input.value = 'untitled.txt'
+                    input.select();
+                },
+                onAccept: name => {
+                    const f = path.join(pwd, name)
+                    fs.closeSync(fs.openSync(f, 'a'))
+                    shell.openItem(f);
+                }
+            })
+        })
 
-    bind('del', ['del'], del).filterMode([])
-    bind('edit', ['f4'], edit).filterMode()
-    bind('newFile', ['shift+f4'], newFile).filterMode()
-    bind('rename', ['f2'], rename).filterMode()
-    bind('newFolder', ['f7'], newFolder).filterMode()
-    bind('mv', ['f6'], mv).filterMode()
+        panel.bind('rename', ['f2'], () => {
+            dialog.open({
+                label: 'Rename',
+                onOpen: input => {
+                    console.log('open', input)
+                    const name = panel.getCurrentItem().name
+                    input.value = name
+                    input.select()
+                    input.setSelectionRange(0, name.length - path.extname(name).length);
+                },
+                onAccept: name => {
+                    fs.renameSync(
+                        panel.getCurrentItem().path,
+                        path.join(panel.getUrl().path, name)
+                    )
+                },
+            })
+        })
 
-    bind('copyFilePath', ['p'], () => {
-        copyPath(activePanel().getCurItem().path)
-    }).filterMode([])
+        panel.bind('newFolder', ['f7'], () => {
+            dialog.open({
+                label: 'New Folder',
+                onOpen: input => {
+                    input.value = 'New Folder'
+                    input.select()
+                },
+                onAccept: name => {
+                    fs.mkdirSync(
+                        path.join(panel.getUrl().path, name)
+                    )
+                },
+            })
+        })
 
-    bind('copyDirPath', ['P'], () => {
-        copyPath(activePanel().getPath())
-    }).filterMode([])
+        panel.bind('move', ['f6'], () => {
+            panel.getSelectedItems().forEach(file =>
+                fs.move(
+                    file.path,
+                    path.join(jumpFm.getPanelPassive().getUrl().path, file.name)
+                )
+            )
+        })
+
+        const copy = (txt) => {
+            clipboard.writeText(txt)
+            jumpFm.statusBar.msg('clipboard')
+                .setType('info')
+                .setText('Copied to Clipboard')
+                .setTooltip(txt)
+                .setClearTimeout(6000)
+        }
+
+        panel.bind('copyFilePath', ['p'], () => {
+            copy(panel.getCurrentItem().path)
+        })
+
+        panel.bind('copyDirPath', ['shift+p'], () => {
+            copy(panel.getUrl().path)
+        })
+    })
 }
